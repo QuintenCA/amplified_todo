e8dacf922dfada1f9b8c11ebf429d3cfd2247a2a
Quinten <quintencallen@gmail.com>
2022-10-01 12:47:41 -0400

    stuff

--- android/app/src/main/kotlin/com/example/amplified_todo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/amplified_todo/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.amplified_todo
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
